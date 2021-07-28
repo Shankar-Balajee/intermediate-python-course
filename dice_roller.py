7da51abc951129ec18f4a61dffb01711b148c268
Shankar-Balajee <80673647+Shankar-Balajee@users.noreply.github.com>
2021-07-28 07:59:30 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
-def main():
-  #print('You rolled a die')
 
+def main():
+  while True:
+    print('you rolled a die')
+    roll=random.randint(1,6)
+    print(f'you rolled a {roll}')
+    
+    
+    
+  
 if __name__== "__main__":
+  import random
   main()
